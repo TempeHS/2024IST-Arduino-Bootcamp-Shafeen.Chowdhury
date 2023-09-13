cda23d147280deae4f8000e3c5697b89cc50279a
Shaf0 <Shafeen.Chowdhury@education.nsw.gov.au>
2023-09-13 14:07:20 +1000

    complete 13

--- 13.I2C/functions.ino
+++ 13.I2C/functions.ino
@@ -2,9 +2,13 @@ void updateOLED (float theTemp) {
   u8g2.clearBuffer();                  // clear the internal memory
   u8g2.setFont(u8g2_font_ncenB08_tr);  // choose a suitable font
   u8g2.setCursor(0, 10);              // Set cursor to first line
-  u8g2.print("[-$-]");         // write something to the internal memory
-  u8g2.setCursor(0, 20);              // Set cursor to first line
-  u8g2.print("the tempreture is: "); 
+    u8g2.print("the tempreture is: ");       // write something to the internal memory                     
   u8g2.print(theTemp);                              // write something to the internal memory
   u8g2.sendBuffer();                  // transfer internal memory to the display
+}
+
+
+
+float sampleTemperature(){
+  return myPressureSensor.getTemperature();
 }
